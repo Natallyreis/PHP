85009fc913db452d28833f22375730832b2db8af
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-21 11:42:28 -0300

    commit

--- index13.php
+++ index13.php
@@ -1,7 +1,7 @@
 <?php
     // Exemplo de concatenação $rua = "Rua do Senai, 1000";
     $bairro = "Jd do aprendizado";
-    $cidade = "Santo Andre"; =
+    $cidade = "Santo Andre"; 
     $cep = "03001-000";
     $enderecocompleto = "$rua ";
     $enderecocompleto .= "$bairro - ";
