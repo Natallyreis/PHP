1d4af2a8aefb361c912ab74c0dee7ab1906d9323
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-21 11:41:23 -0300

    commit

---
+++ index14.php
@@ -0,0 +1,11 @@
+<?php
+    // Exemplo de concatenação
+    $rua = "Rua do Senai";
+    $nr = 1000;
+    $bairro = "Jd do aprendizado";
+    $cidade = "Santo Andre";
+    $cep = "03001-000";
+    $Estado = "São Paulo";
+    echo "Eu moro na {$rua}, nr:{$nr} - CEP: {$cep} no bairro {$bairro}, em Cidade: {$cidade}, Estado de {$Estado}.
+    " ;
+?>
