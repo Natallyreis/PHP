fe55d8508a6857eb9a7e7fb2e2d1fe1bb7b4bb79
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-21 09:47:23 -0300

    commit

---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+    // variavel do tipo String
+    $nome = 'Meu nome 123';
+    echo "$nome";
+    echo "<br";
+
+    //variavel do tipo inteiro 
+    $ano = 2023;
+    echo "$ano";
+    echo "<br";
+
+    //variavel so tipo float
+    $pi = 3.14159265;
+    echo "$pi";
+    echo "<br";
+
+    //variavel do tipo booleano
+    $sim = true;
+    echo "$sim";
+    echo "<br";
+?>
