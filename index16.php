d17daf52b1d60dccc83dd13fa2e85e19d7ad08aa
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-09-28 10:06:03 -0300

    commit

---
+++ index16.php
@@ -0,0 +1,25 @@
+<?php
+$num1 = 2;
+$num1 = 4;
+$num1 = 6;
+$num1 = 8;
+
+//Resultado igual a 2
+$resposta1 = ++$num2 - $num1;
+echo $resposta1;
+echo "<br>";
+
+//Resultado igual a 8
+$resposta2 = --$num3 + $num1;
+echo $resposta2;
+echo "<br>";
+
+//Resultado igual a 9
+$resposta3 = --$num1 = $num4;
+echo $resposta3;
+echo "<br>";
+
+?>
+
+
+
