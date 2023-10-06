8f0be44aa0e9d52fff66518d456899c55e982158
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-10-06 15:22:57 -0300

    commit

---
+++ index30.php
@@ -0,0 +1,8 @@
+<?php
+
+    include "teste.php";
+
+?>
+
+    <p>Após include</p>
+    <p>O conteúdo do programa teste.php é <?php echo $c; ?></p>
---
+++ teste.php
@@ -0,0 +1,7 @@
+<?php
+
+    $c = 5;
+
+?>
+
+<h1>arquivo teste.php com declaração da variável c</h1>
